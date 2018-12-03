a2c758eadf6572b864086d36128a9d3305e64acf
Arjan van Wijk <thanarie@gmail.com>
2018-12-03 12:18:05 +0100

    Cleanup

--- test/Example.spec.ts
+++ test/Example.spec.ts
@@ -1,21 +1,21 @@
-import Uploader from '../src/lib/Uploader';
+// import Uploader from '../src/lib/Uploader';
 import {expect} from 'chai';
 
-let example:Uploader;
+// let example:Uploader;
 
 describe('Example', () => {
   beforeEach(() => {
-    example = new Uploader({
-      localPath: '',
-      remotePath: '',
-      bucket: '',
-    });
+    // example = new Uploader({
+    //   localPath: '',
+    //   remotePath: '',
+    //   bucket: '',
+    // });
   });
 
   it('should return the correct environment', () => {
   });
 
   it('should return the default environment when none has been supplied', () => {
-    expect(example).to.equal('baz');
+    expect('foo').to.equal('foo');
   });
 });
